348b2f699e39fce0c2e2d62673aec489325261d6
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-04-07 17:58:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+from flask import session
+
+def check_role(required_role):
+    """Cek apakah role user di session sesuai dengan required_role"""
+    role = session.get('role')  
+    return role == required_role
+
 
 DB_CONFIG = {
     'host': os.getenv("DB_HOST", "maglev.proxy.rlwy.net"),
