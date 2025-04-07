538272018f1d6970718de8da1bac35f08b9ba3e4
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-04-07 18:05:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ def check_role(required_role):
 
 
 DB_CONFIG = {
-    'host': os.getenv("DB_HOST", "maglev.proxy.rlwy.net"),
-    'dbname': os.getenv("DB_NAME", "railway"),
-    'user': os.getenv("DB_USER", "postgres"),
-    'password': os.getenv("DB_PASSWORD", "JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo"),
-    'port': os.getenv("DB_PORT", 39316)
+    'host': "maglev.proxy.rlwy.net",
+    'dbname': "railway",
+    'user': "postgres",
+    'password': "JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo",
+    'port': 39316
 }
