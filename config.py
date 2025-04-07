8c23ad075005d3b0c9691977dd4b22b634f3d9eb
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-04-07 17:54:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-# config.py
-
-from flask import session
-import redis
 import os
 
-def check_role(required_role):
-    """Cek apakah role user di session sesuai dengan required_role"""
-    role = session.get('role')  
-    return role == required_role
-
-# Tambahkan ini:
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo@maglev.proxy.rlwy.net:39316/railway")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+DB_CONFIG = {
+    'host': os.getenv("DB_HOST", "maglev.proxy.rlwy.net"),
+    'dbname': os.getenv("DB_NAME", "railway"),
+    'user': os.getenv("DB_USER", "postgres"),
+    'password': os.getenv("DB_PASSWORD", "JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo"),
+    'port': os.getenv("DB_PORT", 39316)
+}
