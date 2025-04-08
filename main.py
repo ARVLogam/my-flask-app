6be35ff47790ea75d08cd34ea4466dfeecc8c7ef
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-04-08 17:18:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'secrap7@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pmbqjbiosnuzszt'  # gunakan App Password di sini   
+app.config['MAIL_PASSWORD'] = 'itlukqqxvhkqvuwq'  # gunakan App Password di sini   
 
 mail = Mail(app)
 
