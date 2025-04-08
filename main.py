724dc6cb406a099ae498554037e5e2348f062a6f
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-04-08 15:03:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ from flask_mail import Mail, Message
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('secrap7@gmail.com')  # Email kamu
-app.config['MAIL_PASSWORD'] = os.environ.get('pmbn qjbi osnu zszt')  # Password / App password
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')     
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')      
 
 mail = Mail(app)
 
