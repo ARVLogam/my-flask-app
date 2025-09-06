9940a7b51898a59b94648c116f0f1e5c253fa21d
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-09-06 14:40:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,26 +3,24 @@ from flask import session
 
 def check_role(required_role):
     """Cek apakah role user di session sesuai dengan required_role"""
-    role = session.get('role')  
+    role = session.get('role')
     return role == required_role
 
 
 DB_CONFIG = {
-    'host': "maglev.proxy.rlwy.net",
-    'dbname': "railway",
-    'user': "postgres",
-    'password': "JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo",
-    'port': 39316
+    'host': os.getenv("PGHOST", "maglev.proxy.rlwy.net"),
+    'dbname': os.getenv("PGDATABASE", "railway"),
+    'user': os.getenv("PGUSER", "postgres"),
+    'password': os.getenv("PGPASSWORD", "JxlxNXWerXUEyNLkCgxgBlhSvXKMfNjo"),
+    'port': os.getenv("PGPORT", 39316)
 }
-EMAIL_ADDRESS = 'secrap7@gmail.com'
-EMAIL_PASSWORD = 'itlukqqxvhkqvuwq'
 
 MAIL_SETTINGS = {
     "MAIL_SERVER": "smtp.gmail.com",
     "MAIL_PORT": 587,
     "MAIL_USE_TLS": True,
     "MAIL_USE_SSL": False,
-    "MAIL_USERNAME": "secrap7@gmail.com",
-    "MAIL_PASSWORD": "itlukqqxvhkqvuwq",   # App Password Gmail
-    "MAIL_DEFAULT_SENDER": "secrap7@gmail.com"
+    "MAIL_USERNAME": os.getenv("EMAIL_ADDRESS", "secrap7@gmail.com"),
+    "MAIL_PASSWORD": os.getenv("EMAIL_PASSWORD", "itlukqqxvhkqvuwq"),  # App Password Gmail
+    "MAIL_DEFAULT_SENDER": os.getenv("EMAIL_ADDRESS", "secrap7@gmail.com"),
 }
