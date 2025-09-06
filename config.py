d1869650373727df153c31d7e2765c6cf4caf374
ARVLogam <sembarang.mugoladoh@gmail.com>
2025-09-06 14:25:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,9 +18,12 @@ EMAIL_ADDRESS = 'secrap7@gmail.com'
 EMAIL_PASSWORD = 'itlukqqxvhkqvuwq'
 
 MAIL_SETTINGS = {
-    "MAIL_SERVER": "smtp.gmail.com",
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USERNAME": EMAIL_ADDRESS,
-    "MAIL_PASSWORD": EMAIL_PASSWORD
+MAIL_SERVER=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USE_TLS=true
+MAIL_USE_SSL=false
+MAIL_USERNAME=secrap7@gmail.com
+MAIL_PASSWORD=<APP_PASSWORD_GMAIL>
+MAIL_DEFAULT_SENDER=secrap7@gmail.com
+
 }
